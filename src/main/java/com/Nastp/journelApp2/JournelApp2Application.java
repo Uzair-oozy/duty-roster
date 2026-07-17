b29478fa6e2c4b3dc7d12b3cb56202d828576fa3
Uzair-oozy <oozy004@gmail.com>
2026-07-17 10:15:59 +0500

    init: add Journal App

---
+++ src/main/java/com/Nastp/journelApp2/JournelApp2Application.java
@@ -0,0 +1,13 @@
+package com.Nastp.journelApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournelApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournelApp2Application.class, args);
+	}
+
+}
