defe31a21fdee64e436504a00ed1f3cf48e91785
Uzair-oozy <oozy004@gmail.com>
2026-07-19 16:43:06 +0500

    Addition of RestController and RequestMapping annotation

--- src/main/java/com/Nastp/journelApp2/controller/JournalEntryController.java
+++ src/main/java/com/Nastp/journelApp2/controller/JournalEntryController.java
@@ -1,4 +1,10 @@
 package com.Nastp.journelApp2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+
 }
